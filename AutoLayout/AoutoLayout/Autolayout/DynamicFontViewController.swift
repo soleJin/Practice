5131e2d0af198ed2f2825a23a04a4e5df1d6b54e
Sole <subuchappyv2@gmail.com>
2022-04-26 23:44:56 +0900

    feat: Dynamic Font

---
+++ AutoLayout/AoutoLayout/Autolayout/DynamicFontViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DynamicFontViewController.swift
+//  Autolayout
+//
+//  Created by sole on 2022/04/26.
+//
+
+import UIKit
+
+class DynamicFontViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
